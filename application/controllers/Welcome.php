6467955336e8688ddba6e9dddc785ab053256f03
GitCodeVishal <vkashyap1998@gmail.com>
2024-07-09 23:40:28 +0530

    Done with setup of Admin lte

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$data['page'] = 'index';
+		$this->load->view('template/page',$data);
 	}
 }
