5400dcf8db044051fb5f71389d8558d2a44ea19c
GitCodeVishal <vkashyap1998@gmail.com>
2024-09-03 13:11:29 +0530

    commit message

--- application/views/template/page.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$this->load->view('template/header');
-$this->load->view($page);
-$this->load->view('template/footer');
-
-?>
